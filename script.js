ea395c2add33019ddd3738de4d974d39e086d458
Kyamu <124717126+Kyamu@users.noreply.github.com>
2023-04-20 16:03:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function getFormvalue() {
-    //Write your code here
-
+  var firstName = document.getElementsByName("fname")[0].value;
+  var lastName = document.getElementsByName("lname")[0].value;
+  var fullName = firstName + " " + lastName;
+  alert(fullName);
 }
